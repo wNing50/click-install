794f5ffe90430d5ec91f74bc7e87cc3eed371d9c
Nick Wu <38874640+wNing50@users.noreply.github.com>
2025-05-08 14:17:38 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# ext-name
+
+## Configurations
+
+## Commands
+
+## License
+
+[MIT](./LICENSE.md) License © 2025 [Nick Wu]
---
+++ tsdown.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'tsdown'
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  format: ['cjs', 'esm'],
+  shims: false,
+  dts: false,
+  clean: true,
+  external: [
+    'vscode',
+  ],
+})
